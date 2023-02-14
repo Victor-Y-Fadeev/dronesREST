f3ad2ebe408a7174c8ae32e5499b34b4b3312926
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2023-02-15 01:22:12 +0300

    Add new providers

--- src/test/java/com/musala/dispatcher/DroneProvider.java
+++ src/test/java/com/musala/dispatcher/DroneProvider.java
@@ -3,6 +3,7 @@ package com.musala.dispatcher;
 import com.musala.dispatcher.entity.Drone;
 import com.musala.dispatcher.entity.Model;
 import com.musala.dispatcher.entity.State;
+import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -36,7 +37,26 @@ public class DroneProvider {
                                 + drone.getBatteryCapacity()));
     }
 
-    public static Stream<List<Drone>> provideFilteringDrones() {
+    public static Stream<Arguments> provideFilteringDronesByState() {
+        return provideFilteringDrones()
+                .flatMap(list -> Arrays.stream(State.values())
+                        .map(state -> Arguments.of(list, state)));
+    }
+
+    public static Stream<Arguments> provideFilteringDronesByBatteryCapacity() {
+        return provideFilteringDrones()
+                .flatMap(list -> Stream.of(0, 25, 100)
+                        .map(batteryCapacity -> Arguments.of(list, batteryCapacity)));
+    }
+
+    public static Stream<Arguments> provideFilteringDronesByStateAndBatteryCapacity() {
+       return provideFilteringDrones()
+               .flatMap(list -> Arrays.stream(State.values())
+                       .flatMap(state -> Stream.of(0, 25, 100)
+                               .map(batteryCapacity -> Arguments.of(list, state, batteryCapacity))));
+    }
+
+    private static Stream<List<Drone>> provideFilteringDrones() {
         return provideDrones()
                 .collect(Collectors.groupingBy(Drone::getModel,
                         Collectors.groupingBy(Drone::getWeightLimit)))
