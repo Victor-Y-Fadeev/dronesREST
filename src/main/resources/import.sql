4bbc364144fff7b8e9e12409f73a67da75d66250
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2023-02-20 00:44:44 +0300

    Upload medications

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,20 @@ VALUES
 	('9', 'L', 50, 70, 3),
 	('10', 'M', 150, 50, 4),
 	('11', 'C', 300, 65, 5);
+
+INSERT INTO medications
+	(name, weight, code, image)
+VALUES
+	('Pentalgin', 30, '0', NULL),
+	('Mildronat', 250, '1', NULL),
+	('Carsil', 35, '2', NULL),
+	('Sinupret', 100, '3', NULL),
+	('Phenibut', 250, '4', NULL),
+	('Isoprinosine', 500, '5', NULL),
+	('Enterosgel', 225, '6', NULL),
+	('Pimafucin', 100, '7', NULL),
+	('Clotrimazole', 10, '8', NULL),
+	('Perineva', 75, '9', NULL),
+	('Egilok', 25, '10', NULL),
+	('Enap-H', 35, '11', NULL),
+	('Aquadetrim', 15, '12', NULL);
